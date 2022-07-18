409bec869e45c0434f49a9a9eac0223663825a48
Asro <georgeasro69@gmail.com>
2022-07-18 20:53:04 +0300

    Event loop simplified version

---
+++ EVENT LOOP/event_loop_simplified.js
@@ -0,0 +1,30 @@
+// event loop queue FIFO
+let eventLoop = [];
+let _event;
+
+eventLoop.push(callback1, callback2, callback3);
+
+
+// keep going until all callbacks are processed
+while (eventLoop.length > 0 ) {
+  _event = eventLoop.shift();
+  eventLoop = eventLoop.slice();
+
+  try {
+    _event();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+// callback to be added on event loop
+function callback1() {
+  console.log("CB 1");
+}
+function callback2() {
+  console.log("CB 2");
+}
+function callback3() {
+  console.log("CB 3");
+}
